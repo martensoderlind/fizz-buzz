9feb176f9a751c27190d816cd4c7e32d2adbc4ce
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2025-02-05 15:00:31 +0100

    updated version with match

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,14 @@ fn main() {
     fizz_buzz(100);
 }
 
-fn fizz_buzz(n: i32) -> () {
+fn fizz_buzz(n: i32) {
     if n > 0 {
         for i in 1..=n {
-            if i % 3 == 0 && i % 5 == 0 {
-                println!("FizzBuzz")
-            } else if i % 3 == 0 {
-                println!("Fizz")
-            } else if i % 5 == 0 {
-                println!("Buzz")
-            } else {
-                println!("{}", i)
+            match (i % 3, i % 5) {
+                (0, 0) => println!("fizzbuzz"),
+                (0, _) => println!("fizz"),
+                (_, 0) => println!("buzz"),
+                (_, _) => println!("{}", i),
             }
         }
     }
